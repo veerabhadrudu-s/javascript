7e3552d56189d8a5c4e70ec2654653bacbcc3ab6
S <veerabhadrudu.s@hpe.com>
2019-06-09 12:54:55 +0530

    Basic Programs

---
+++ basics/primitive_data_types.js
@@ -0,0 +1,9 @@
+// String data type
+var name = "Veera";
+//Number data type
+var age = 31;
+// Boolean data type
+var isSummer = true;
+
+console.log(name == "Veera" ? "Strings Matched" : "Strings Not Matched");
+console.log(name == new String("Veera") ? "Strings Matched using new operator also" : "Strings Not Matched");
